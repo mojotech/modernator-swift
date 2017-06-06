90632576ce364e883f1da717c7fea100599f9535
Ryan <ryanmjt@users.noreply.github.com>
2017-06-06 16:50:50 -0400

    Hash passwords on user creation
    This has better handling of JSON fields (sending proper bad requests on
    missing keys) and slightly nicer syntax perhaps.
    
    Used https://github.com/vapor/auth-template/blob/eac76af346283d69b26e539a25a2a21593989841/Sources/App/Routes.swift#L37
    as a reference for the implementation.

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -3,10 +3,32 @@ import HTTP
 
 /// Here we have a controller that helps facilitate the /user endpoint
 final class UserController {
+
+    let hash: HashProtocol
+
+    init(_ hash: HashProtocol) {
+        self.hash = hash
+    }
+
     // Create a new user
     func create(_ request: Request) throws -> ResponseRepresentable {
-        let json = request.json!
-        let user = User(username: try json.get("userName"), password: try json.get("userPassword"))
+        guard let json = request.json else {
+            throw Abort(.badRequest)
+        }
+
+        guard let username = json["userName"]?.string else {
+            throw Abort(.badRequest)
+        }
+        guard let password = json["userPassword"]?.string else {
+            throw Abort(.badRequest)
+        }
+
+        // TODO: better error message if username already exists
+
+        let user = User(
+            username: username,
+            password: try hash.make(password.makeBytes()).makeString()
+        )
         try user.save()
         return Response(status: .ok)
     }
@@ -27,5 +49,3 @@ final class UserController {
         return json
     }
 }
-
-extension UserController: EmptyInitializable { }
--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -23,7 +23,7 @@ extension Droplet {
         let persistMiddleware = PersistMiddleware(User.self)
         let authed = grouped(persistMiddleware)
 
-        let userController = UserController()
+        let userController = UserController(self.hash)
         post("users", handler: userController.create)
         post("users/login", handler: userController.login)
         authed.get("users/me", handler: userController.me)
